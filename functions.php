aa1ced35ea9804b3765f23665ab1bbc9100c38b6
Anna <annaatiagina@Annas-MBP.PK5001Z>
2015-06-12 00:52:16 -0700

    SEO and widgets

--- functions.php
+++ functions.php
@@ -15,8 +15,54 @@ register_nav_menus(array('main-menu' => __( 'Main' ),
 
 //End register my menus
 
+//Register Sidebar
+
+register_sidebar(array(
+    'before_widget' => '<div>',
+    'after_widget' => '</div>',
+'before_title'=> '<h3>',
+'after_title' => '</h3>'   
+));
+
+//end register sidebar
+
 
 //enable featured images and post thumbnails
 
 add_theme_support('post-thumbnails');
-?>
+
+//add post type support
+
+add_post_type_support('page', 'excerpt');
+
+//title function
+
+function get_my_title_tag(){
+
+    global $post; //DO NOT forget it
+    
+    if (is_front_page()){ //the front page 
+        bloginfo('description');
+    
+    }elseif (is_page() || is_single()) { //pages and postings
+    
+    the_title(); //title of the page or posting
+        
+    }else {
+    
+      bloginfo('description');  
+    
+    }
+    
+    if($post->post_parent){ //if there is a parent page
+    
+        echo ' | '; //separator with spaces
+        echo get_the_title($post->post_parent); //title of a parent page
+    }
+    
+    echo ' | '; //separator with spaces
+    bloginfo('name'); //site name
+    echo ' | '; //separator with spaces
+    echo 'Seattle, WA'; //city and state
+
+};
--- sidebar-left.php
+++ sidebar-left.php
@@ -26,5 +26,19 @@ wp_list_pages(array('child_of' => $post->ID, 'title_li' => __('')));
 <?php wp_list_categories( array('title_li' => '')); ?>
        
        <?php endif; ?>
+    <!-- Start Quote -->
+    
+<?php if(get_post_meta($post->ID, 'Quote', true)) : ?>
+<blockquote><?php echo get_post_meta($post->ID, 'Quote', true); ?> </blockquote>    
+<?php endif; ?>    
+    
+    <!-- Begin dynamic sidebar -->
+    <div class="widget">
+    <?php dynamic_sidebar(); ?>
+    </div>
+     <!-- end dynamic sidebar -->
+
+<!-- End Quote -->
+    
         </aside><!-- /primary -->
        
